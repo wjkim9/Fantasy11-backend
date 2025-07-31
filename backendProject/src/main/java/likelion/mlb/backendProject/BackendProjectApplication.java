7102c1fd084f53e9d17472e3dd450497eafad6f8
hwichoi0317 <chowko@naver.com>
2025-07-31 10:42:32 +0900

    springBoot프로젝트 최초 commit

---
+++ backendProject/src/main/java/likelion/mlb/backendProject/BackendProjectApplication.java
@@ -0,0 +1,13 @@
+package likelion.mlb.backendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectApplication.class, args);
+	}
+
+}
