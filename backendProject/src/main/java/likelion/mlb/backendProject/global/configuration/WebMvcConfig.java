cb6c23188543541cdf9a899dd9d6247109f25a95
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2025-08-08 16:12:50 +0900

    feat: 프론트 연동 CORS 문제 해결 #29

---
+++ backendProject/src/main/java/likelion/mlb/backendProject/global/configuration/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package likelion.mlb.backendProject.global.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String url;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(url)
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
