afce02bc029103255edc44d6b44d90a96388600f
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2025-08-08 16:27:29 +0900

    feat: 프론트 연동 CORS 문제 해결 #29

--- backendProject/src/main/java/likelion/mlb/backendProject/global/configuration/WebMvcConfig.java
+++ backendProject/src/main/java/likelion/mlb/backendProject/global/configuration/WebMvcConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${frontend.http.url}")
